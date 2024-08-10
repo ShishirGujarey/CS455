7a3083cff479c5bac56ed8c437735bcb66190600
aryanm17 <aryanm21@iitk.ac.in>
2024-08-11 04:56:06 +0530

    1-File Structure Definition 2-CSS Styling 3-JS Setup 4-drawBox fuction 5-drawGrid function 6-Game State 7- upgradeGrid function 8- keyboardEvents function

---
+++ src/index.js
@@ -0,0 +1,78 @@
+const state = {
+    secret: dictionary[Math.floor(Math.random() * dictionary.length)],
+    grid: Array(6)
+        .fill()
+        .map(() => Array(5).fill('')),
+    currentRow: 0,
+    currentCol: 0,
+};
+
+function updateGrid() {
+    for (let i = 0; i < state.grid.length; i++) {
+        for (let j = 0; j < state.grid[i].length; j++) {
+            const box = document.getElementById(`box${i}${j}`);
+            box.textContent = state.grid[i][j];
+        }
+    }
+}
+
+function drawBox(container, row, col, letter = '') {
+    const box = document.createElement('div');
+    box.className = 'box';
+    box.textContent = letter;
+    box.id = `box${row}${col}`;
+
+    container.appendChild(box);
+    return box;
+}
+
+function drawGrid(container) {
+    const grid = document.createElement('div');
+    grid.className = 'grid';
+
+    for (let i = 0; i < 6; i++) {
+        for (let j = 0; j < 5; j++) {
+            drawBox(grid, i, j);
+        }
+    }
+
+    container.appendChild(grid);
+}
+
+
+
+
+function registerKeyboardEvents() {
+    document.body.onkeydown = (e) => {
+        const key = e.key;
+        if (key === 'Enter') {
+            if (state.currentCol === 5) {
+                const word = getCurrentWord();
+                if (isWordValid(word)) {
+                    revealWord(word);
+                    state.currentRow++;
+                    state.currentCol = 0;
+                } else {
+                    alert('Not a valid word.');
+                }
+            }
+        }
+        if (key === 'Backspace') {
+            removeLetter();
+        }
+        if (isLetter(key)) {
+            addLetter(key);
+        }
+
+        updateGrid();
+    };
+}
+
+function startup() {
+    const game = document.getElementById('game');
+    drawGrid(game);
+
+    registerKeyboardEvents();
+}
+
+startup();
